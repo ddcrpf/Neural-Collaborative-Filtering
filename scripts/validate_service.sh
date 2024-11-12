574d7a64315194cb52f2724f14c7571a2765530b
Prnjl <101033868+ddcrpf@users.noreply.github.com>
2024-11-13 03:17:25 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo "Validating service..."
-systemctl status httpd || systemctl status apache2 || exit 1
+echo "Validating Flask application..."
+pgrep -f app.py > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Flask application is not running."
+    exit 1
+else
+    echo "Flask application is running."
+fi

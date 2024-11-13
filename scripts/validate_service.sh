f27c5ff8065cc786ca0497743ba0ec2b9ae4965b
Prnjl <101033868+ddcrpf@users.noreply.github.com>
2024-11-13 19:25:29 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-echo "Validating Flask application..."
-pgrep -f app.py > /dev/null
-if [ $? -ne 0 ]; then
+set -ex
+
+echo "Validating Flask application on Amazon Linux..."
+
+# Check if the Flask application process is running
+if pgrep -f app.py > /dev/null; then
+    echo "Flask application is running."
+else
     echo "Flask application is not running."
     exit 1
-else
-    echo "Flask application is running."
 fi
